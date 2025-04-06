b5be8484aaeb42f7ee31e837be27da0068b773b8
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-06 18:01:37 +0200

    initial commit

---
+++ src/main/java/dev/marta_bernardo/party_escape/PartyEscapeApplication.java
@@ -0,0 +1,13 @@
+package dev.marta_bernardo.party_escape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyEscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyEscapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marta_bernardo/party_escape/PartyEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marta_bernardo.party_escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyEscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
