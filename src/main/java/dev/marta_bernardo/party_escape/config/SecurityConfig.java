40c885907ae2a9658e6b5bb3cd8922d20a1dc8c8
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-17 12:35:50 +0200

    SecurityConfig with permitAll created

---
+++ src/main/java/dev/marta_bernardo/party_escape/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package dev.marta_bernardo.party_escape.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
