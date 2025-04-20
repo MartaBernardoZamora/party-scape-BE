675f833f4c2cde38d92323a12108771cf749c311
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-20 11:07:48 +0200

    Implement SecurityConfig with all permissions granted

--- src/main/java/dev/marta_bernardo/party_escape/config/SecurityConfig.java
+++ src/main/java/dev/marta_bernardo/party_escape/config/SecurityConfig.java
@@ -11,7 +11,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/api/v1/**").permitAll()
                 .anyRequest().permitAll()
             )
             .csrf(csrf -> csrf.disable());
