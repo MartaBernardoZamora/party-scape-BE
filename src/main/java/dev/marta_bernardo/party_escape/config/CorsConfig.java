e7eaf41ff289ea1663e2afc3f540b17b60235312
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-04-27 19:51:25 +0200

    Change cors to https front

--- src/main/java/dev/marta_bernardo/party_escape/config/CorsConfig.java
+++ src/main/java/dev/marta_bernardo/party_escape/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
