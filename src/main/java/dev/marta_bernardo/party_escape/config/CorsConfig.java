27f72d7a176aae351400afd1814f3ee5163f1b9c
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-06-11 15:05:14 +0200

    feat: configure CORS to allow GitHub Pages origin using environment variable

--- src/main/java/dev/marta_bernardo/party_escape/config/CorsConfig.java
+++ src/main/java/dev/marta_bernardo/party_escape/config/CorsConfig.java
@@ -4,16 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class CorsConfig {
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://localhost:5173")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
